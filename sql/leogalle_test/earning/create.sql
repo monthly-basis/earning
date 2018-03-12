2200cf53a2bf07030e6897761b50ff7bada6d7b4
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-12 10:53:19 -0700

    Add type_id column to earning table

--- sql/leogalle_test/earning/create.sql
+++ sql/leogalle_test/earning/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `earning` (
     `earning_id` int(10) unsigned auto_increment,
     `user_id` int(10) unsigned not null,
+    `entity_id` int(10) default null,
     `entity_type_id` int(10) unsigned not null,
-    `entity_id` int(10) unsigned not null,
+    `type_id` int(10) unsigned not null,
     `amount` float(4,2) not null,
     `created` datetime not null,
     PRIMARY KEY (`earning_id`),
